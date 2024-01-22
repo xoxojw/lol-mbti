be6822b601cb21cf6fd7241e107c73e6c17e1f4b
Jiwon Park <xoxojw.dev@gmail.com>
2024-01-23 00:36:32 +0900

    style: footer padding 조정 - 모바일 확인 테스트

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
 	return (
-		<footer className="absolute bottom-0 text-stone-200 lg:text-stone-400 pb-14 text-sm flex flex-col items-center gap-1">
+		<footer className="absolute bottom-0 text-stone-200 lg:text-stone-400 pb-10 text-sm flex flex-col items-center gap-1">
 			<span>© 2024 JW All rights reversed</span>
 			<span>Used Riot Games' materials</span>
 		</footer>
