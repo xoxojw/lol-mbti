79838a2c4f01c4faa540df6cc8a6035ce905ae53
Jiwon Park <xoxojw.dev@gmail.com>
2024-09-30 22:54:16 +0900

    chores: 메인페이지 수정

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,12 +1,21 @@
 import Introduce from "components/Introduce";
 import MetaData from "components/meta/MetaData";
+import summonersRift from "assets/images/summoners_rift.webp";
 
 const Main = () => {
 	return (
-		<div className="flex flex-col justify-center items-center w-screen h-screen bg-summonersRift bg-no-repeat bg-center" role="img">
-			<MetaData />
-			<Introduce />
-		</div>
+		<main className="w-screen h-screen flex flex-col">
+			<img
+				src={summonersRift}
+				alt="소환사의 협곡 배경 이미지"
+				className="fixed -z-10 w-screen h-screen md:object-contain object-cover"
+				loading="eager"
+			/>
+			<div className="flex flex-col justify-center items-center w-screen h-screen">
+				<MetaData />
+				<Introduce />
+			</div>
+		</main>
 	);
 };
 
